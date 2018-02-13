b497a94d95e73ac2e5597b4db47cecfa4ee36830
Qasim Iqbal <iqbalqasim46@gmail.com>
2018-02-13 11:45:08 -0500

    Added CardGame project DLL which includes all the files. (not any code in them yet)

--- Game/CardGame/HumanPlayer.cs
+++ Game/CardGame/HumanPlayer.cs
@@ -4,10 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Game
+namespace CardGame
 {
-    class Player
+    class HumanPlayer
     {
-        // Abs
     }
 }
