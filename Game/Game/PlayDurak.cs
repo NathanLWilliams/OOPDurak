474db97d2d63cf1b0b68ffc0d7dff80d4843b22d
Nathan <Nathan@NATHAN_PC>
2018-03-12 11:04:39 -0400

    Adding more styling to the main menu, started on adding the statistics panel of the main menu

--- Game/Game/PlayDurak.cs
+++ Game/Game/PlayDurak.cs
@@ -12,10 +12,12 @@ namespace Game
 {
     public partial class PlayDurak : Form
     {
+        //BUGS:
+
         //TODO: Maybe add some static font sizes or buttons sizes which are shared and consistent between each layout/state
 
         //Separate the different sets of controls/layouts into panels so we can easily loop through in code to set visible or invisible
-        public static Panel mainMenuPanel;
+        public static TableLayoutPanel mainMenuPanel;
 
         public enum GameState
         {
@@ -58,7 +60,16 @@ namespace Game
         /// </summary>
         public void GenerateMainMenu()
         {
-            mainMenuPanel = new Panel();
+            mainMenuPanel = new TableLayoutPanel();
+            mainMenuPanel.ColumnCount = 1;
+            mainMenuPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            mainMenuPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            mainMenuPanel.RowCount = 2;
+            mainMenuPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6F));
+            mainMenuPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 94F));
+
+            Panel mainContent = new Panel();
+            Panel statistics = new Panel();
 
             //Variables
             const int BUTTON_WIDTH = 300;
@@ -74,18 +85,32 @@ namespace Game
             Label lblTitle = new Label();
             lblTitle.Text = "Play Durak";
             lblTitle.Font = new Font("Arial", 64, FontStyle.Bold);
-            lblTitle.Location = new Point(300, 150);
+            lblTitle.Location = new Point(this.Width/2 - lblTitle.Width, 150);
+            lblTitle.ForeColor = Color.LimeGreen;
             lblTitle.AutoSize = true;
-            mainMenuPanel.Controls.Add(lblTitle);
+            lblTitle.BackColor = Color.Transparent;
+            mainContent.Controls.Add(lblTitle);
 
             //Instantiate the controls
-            mainMenuPanel.Controls.AddRange(GetButtonMenu(buttonText, 
+            mainContent.Controls.AddRange(GetButtonMenu(buttonText, 
                 new Size(BUTTON_WIDTH, BUTTON_HEIGHT), 
                 new Point(BUTTON_INITIAL_X, BUTTON_INITIAL_Y), 
                 BUTTON_VERTICAL_SPACING, BUTTON_HORIZONTAL_SPACING,
-                new Font("Arial", 24, FontStyle.Regular)));
+                new Font("Arial", 24, FontStyle.Regular),
+                Color.LimeGreen));
+            mainContent.BackgroundImage = Properties.Resources.mainMenuCombinedBackground;
+            mainContent.BackgroundImageLayout = ImageLayout.Stretch;
+            mainContent.Dock = DockStyle.Fill;
+            statistics.Dock = DockStyle.Fill;
 
-            mainMenuPanel.Dock = DockStyle.Fill;
+            Label winsLabel = new Label();
+            winsLabel.Text = "Wins: ";
+            winsLabel.AutoSize = true;
+            winsLabel.Location = new Point(10, 10);
+            statistics.Controls.Add(winsLabel);
+
+            mainMenuPanel.Controls.Add(statistics, 0, 0);
+            mainMenuPanel.Controls.Add(mainContent, 0, 1);
             this.Controls.Add(mainMenuPanel);
 
         }
@@ -99,13 +124,15 @@ namespace Game
         /// <param name="verticalSpacing">The vertical spacing between each button</param>
         /// <param name="horizontalSpacing">The horizontal spacing between each button</param>
         /// <returns>An array of buttons</returns>
-        public static Button[] GetButtonMenu(string[] buttonText, Size buttonSize, Point buttonLocation, int verticalSpacing=25, int horizontalSpacing=25, Font font = default(Font))
+        public static Button[] GetButtonMenu(string[] buttonText, Size buttonSize, Point buttonLocation, int verticalSpacing=25, int horizontalSpacing=25, Font font = default(Font), Color backgroundColor = default(Color), FlatStyle style = FlatStyle.Popup)
         {
             Button[] menuOptions = new Button[buttonText.Length];
             for (int i = 0; i < buttonText.Length; i++)
             {
                 Button btnTemp = new Button();
                 btnTemp.Text = buttonText[i];
+                btnTemp.BackColor = backgroundColor;
+                btnTemp.FlatStyle = style;
 
                 //Determine if either axis of spacing is used, and if so make sure to include the button width or height
                 int width = horizontalSpacing == 0 ? 0 : buttonSize.Width;
