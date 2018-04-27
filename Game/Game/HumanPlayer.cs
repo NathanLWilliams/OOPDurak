722eea5bd1cdc875937950bf63925096b40f9f7c
jhermans-dc <35115202+jhermans-dc@users.noreply.github.com>
2018-04-26 20:42:08 -0400

    Update HumanPlayer.cs

--- Game/Game/HumanPlayer.cs
+++ Game/Game/HumanPlayer.cs
@@ -1,4 +1,9 @@
-﻿using System;
+/* HumanPlayer.cs
+ * Group 9 (Nathan Williams, Jonathan Hermans, Karence Ma, Qasim Iqbal)
+ * Date: 26/4/18
+ * Description: A class for human players.
+ */
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,12 +11,18 @@ using System.Threading.Tasks;
 
 namespace CardGame
 {
+    /// <summary>
+    /// Inherits from the abstract class Player
+    /// </summary>
     public class HumanPlayer : Player
     {
 
+        /// <summary>
+        /// Some functionality only available to player maybe?
+        /// </summary>
         public void Pass()
         {
-            // some functionality only available to player maybe?
+
         }
     }
 }
